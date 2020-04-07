fd522ec29b56039bd043e26a103888f6d4249642
shally5991 <shailja5991@gmail.com>
2020-04-07 17:46:56 +0530

    implemented firebase utils , included ability to store authenticated users into our firebase database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,7 +3,6 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    
     apiKey: "AIzaSyC6jgWBDOdK3H21Q6kcK9_OMHfsvMlzzyQ",
     authDomain: "crwn-db-44120.firebaseapp.com",
     databaseURL: "https://crwn-db-44120.firebaseio.com",
@@ -17,15 +16,36 @@ const config = {
 
 firebase.initializeApp(config);
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
-const provider =  new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({prompt:'select_account'});
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
-
-
-
-
